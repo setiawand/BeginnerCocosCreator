d769856806b919cd77f8e4632320b6c58f58967b
Deni Setiawan <deni.setiawan@live.com>
2020-09-20 10:55:06 +0700

    Cocos Creator Bagi Pemula - Scripting

--- assets/Script/HelloWorld.js
+++ assets/Script/HelloWorld.js
@@ -6,6 +6,14 @@ cc.Class({
             default: null,
             type: cc.Label
         },
+        cocosCreatorBtnLabel: {
+            default: null,
+            type: cc.Label
+        },
+        isRockBtnLabel: {
+            default: null,
+            type: cc.Label
+        },
         // defaults, set visually when attaching this script to the Canvas
         text: 'Hello, World!'
     },
@@ -19,4 +27,14 @@ cc.Class({
     update: function (dt) {
 
     },
+
+    onCocosCreatorButtonClicked: function() {
+        cc.log('CocosCreatorButtonClicked')
+        this.label.string = this.cocosCreatorBtnLabel.string
+    },
+
+    onIsRockButtonClicked: function() {
+        cc.log('IsRockButtonClicked')
+        this.label.string = this.isRockBtnLabel.string;
+    }
 });
